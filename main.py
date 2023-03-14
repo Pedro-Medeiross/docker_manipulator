3c31d116b6db3998abbb9cbb845eac47f57d0540
Pedro Medeiros <pedro.contato166@hotmail.com>
2023-03-14 02:37:30 -0300

    Adding Cors

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import docker
 import repository
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 client = docker.from_env()
 
