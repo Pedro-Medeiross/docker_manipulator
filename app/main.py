9d5db8505711d7d61d6d0444ee828341f7a89f37
pedro <pedro.contato166@hotmail.com>
2023-04-07 09:09:53 -0300

    adjusting bot main file

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 import os
-import time
+import asyncio
 import api
 from iqoption import IqOption
-import asyncio
 
 # Pares que serão monitorados
 monitored_pairs = []
@@ -18,7 +17,6 @@ Iq = IqOption(user_id)
 Iq.connect()
 Iq.set_account_type()
 print('Conectado à API')
-instance = Iq.instance()
 
 # Status do bot
 status_bot = asyncio.run(api.get_status_bot(user_id))
@@ -37,47 +35,40 @@ async def update_monitored_pairs(user_id: int):
     # Obtém os pares de moedas disponíveis para negociação
     trade_info_pairs = await api.get_trade_info_pairs(user_id)
     # Adiciona os pares que ainda não estão sendo monitorados
-    for par in trade_info_pairs:
-        if par not in monitored_pairs:
-            monitored_pairs.append(par)
-            print('Monitorando par: ', par)
+    for pair in trade_info_pairs:
+        if pair not in monitored_pairs:
+            monitored_pairs.append(pair)
+            print('Monitorando par: ', pair)
 
 
-async def get_candles(pair):
+async def monitor_pair(pair: str):
+    print('Verificando par: ', pair)
     if pair not in candles_streams:
-        candles_streams[pair] = await instance.subscribe_candles(pair, 1)
+        candles_streams[pair] = Iq.start_candles_stream(pair, 60, 5)
     async for candle in candles_streams[pair]:
-        print(f'par: {pair} candle: {candle["open"]}')
+        print(f'par: {pair} candle: {candle}')
         for trade_info_id in trade_info_ids:
             trade_info = await api.get_trade_info(trade_info_id)
             user_values = await api.get_user_values_by_trade_id(trade_info_id)
             price = trade_info['price']
             action = trade_info['method']
             time_frame = trade_info['timeframe']
-            amount = user_values['ammount']
+            amount = user_values['amount']
             trade_status = user_values['status']
-            if candle['open'] == price:
+            if candle["close"] == price:
                 if trade_status == 0:
                     print('Comprando', pair, 'com valor de', price, 'em', time_frame, 'minutos', )
-                    await instance.buy_digital_spot(active=pair, amount=amount, action=action,
-                                                  duration=time_frame)
+                    await Iq.buy_digital_spot(active=pair, amount=amount, action=action, duration=time_frame)
                     await api.set_schedule_status(trade_info_id, status=1, user_id=user_id)
-
-
-async def monitor_pairs():
-    while status_bot == 1:
-        await update_monitored_pairs(user_id)
-        for pair in monitored_pairs:
-            print('Verificando par: ', pair)
-            asyncio.create_task(get_candles(pair))
-        await asyncio.sleep(5)
 
 
 async def main():
     global trade_info_ids
-    trade_info_ids = await api.get_trade_user_info_scheduled(user_id)
-    while True:
-        await asyncio.gather(monitor_pairs(), asyncio.sleep(5))
+    while status_bot == 1:
+        await update_monitored_pairs(user_id)
+        tasks = [monitor_pair(pair) for pair in monitored_pairs]
+        await asyncio.gather(*tasks)
+        trade_info_ids = await api.get_trade_user_info_scheduled(user_id)
 
-
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
