08f273eeb3ea8c096ad80753d81987b11ca3c383
Pedro Medeiros <pedro.contato166@hotmail.com>
2023-04-13 18:33:59 -0300

    Adjusting buy time

--- app/main.py
+++ app/main.py
@@ -99,19 +99,19 @@ async def buy_trade(trade_info_id: int):
                         remaining3 = instance.get_remaning(3)
                         remaining5 = instance.get_remaning(5)
                         print(f'Verificando tempo restante para compra de binário: {remaining2}, {remaining3}, {remaining5}')
-                        if 90 < remaining2 < 120 and time_frame == 1.3:
+                        if 90 < remaining2 < 150 and time_frame == 2:
                             print(
                                 f'Comprando Binário {pair} com valor de {price} em {time_frame} minutos, com range de {candle}, {zone1}, {zone2}')
                             instance.buy(price=amount, ACTIVES=pair, expirations=time_frame, ACTION=action)
                             await(api.set_schedule_status(trade_id=trade_info_id, status=1, user_id=user_id))
                             await(api.set_trade_associated_exited_if_buy(trade_info_id))
-                        if 120 < remaining3 < 180 and time_frame == 3:
+                        if 120 < remaining3 < 210 and time_frame == 3:
                             print(
                                 f'Comprando Binário {pair} com valor de {price} em {time_frame} minutos, com range de {candle}, {zone1}, {zone2}')
                             instance.buy(price=amount, ACTIVES=pair, expirations=time_frame, ACTION=action)
                             await(api.set_schedule_status(trade_id=trade_info_id, status=1, user_id=user_id))
                             await(api.set_trade_associated_exited_if_buy(trade_info_id))
-                        if 270 < remaining5 < 300 and time_frame == 5:
+                        if 270 < remaining5 < 330 and time_frame == 5:
                             print(
                                 f'Comprando Binário {pair} com valor de {price} em {time_frame} minutos, com range de {candle}, {zone1}, {zone2}')
                             instance.buy(price=amount, ACTIVES=pair, expirations=time_frame, ACTION=action)
@@ -137,7 +137,7 @@ async def buy_trade(trade_info_id: int):
                         remaining3 = instance.get_remaning(3)
                         remaining5 = instance.get_remaning(5)
                         print(f'Verificando tempo restante para compra de binário: {remaining2}')
-                        if 90 < remaining2 < 150 and time_frame == 1.3:
+                        if 90 < remaining2 < 150 and time_frame == 2:
                             print(
                                 f'Comprando Binário {pair} com valor de {price} em {time_frame} minutos, com range de {candle}, {zone1}, {zone2}')
                             instance.buy(price=amount, ACTIVES=pair, expirations=time_frame, ACTION=action)
